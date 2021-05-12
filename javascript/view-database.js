d30b3d8674f68db274958f7d0bbed92e573fb975
Bina Mircea <bina.mircea2001@gmail.com>
2021-05-12 13:31:28 +0300

    view database bage with sort and delete

--- javascript/view-database.js
+++ javascript/view-database.js
@@ -1,3 +1,19 @@
+const monthNumber = {
+    'Jan': '01',
+    'Feb': '02',
+    'Mar': '03',
+    'Apr': '04',
+    'May': '05',
+    'Jun': '06',
+    'Jul': '07',
+    'Aug': '08',
+    'Sep': '09',
+    'Oct': '10',
+    'Nov': '11',
+    'Dec': '12'
+};
+
+// Fetches and sorts reservations
 function fetchReservations() {
     fetch('http://localhost:3000/busy-time-intervals', {
         method: 'get'
@@ -9,24 +25,41 @@ function fetchReservations() {
             const busyIntervalsDiv = document.getElementById("busy-time-intervals-div");
             busyIntervalsDiv.innerHTML = "";
 
+            // Sorting by date
+            data.sort(function (a, b) {
+
+                if (a["year"] != b["year"]) {
+                    return a["year"] - b["year"];
+                }
+
+                if (a["month"] != b["month"]) {
+                    return monthNumber[a["month"] - monthNumber[b["month"]]]
+                }
+
+                if (a["day"] != b["day"]) {
+                    return a["day"] - b["day"];
+                }
+
+                return a["start-hour"] - b["start-hour"]
+            })
+
             data.forEach(reservation => {
 
                 let nameElement = document.createElement("div");
-                pElement.setAttribute("class", "name");
-                pElement.innerText = reservation["customer-name"];
+                nameElement.setAttribute("class", "name");
+                nameElement.innerText = reservation["customer-name"];
 
                 let emailElement = document.createElement("div");
-                pElement.setAttribute("class", "email");
-                pElement.innerText = reservation["email"];
+                emailElement.setAttribute("class", "email");
+                emailElement.innerText = reservation["email"];
 
-                let dateElement = document.createElement(div);
-                
-                    " with email: " + reservation["email"] +
-                    " for " + reservation["day"] + " " + reservation["month"] + " " + reservation["year"] +
-                    " at " + reservation["start-hour"];
+                let dateElement = document.createElement("div");
+                dateElement.setAttribute("class", "date");
+                dateElement.innerText = reservation["day"] + " " + reservation["month"] + " " + reservation["year"] + " at " + reservation["start-hour"];
 
-                console.log(pElement);
-                busyIntervalsDiv.appendChild(pElement);
+                busyIntervalsDiv.appendChild(nameElement);
+                busyIntervalsDiv.appendChild(emailElement);
+                busyIntervalsDiv.appendChild(dateElement);
             });
 
         })
@@ -34,3 +67,44 @@ function fetchReservations() {
 }
 
 window.addEventListener('DOMContentLoaded', () => fetchReservations());
+
+// Removes past reservations from the database (only works one at a time?)
+function removePastReservations() {
+
+
+    fetch('http://localhost:3000/busy-time-intervals', {
+        method: 'get'
+    }).then(function (response) {
+        response.json().then((data) => {
+            
+            let responses = 0;
+            let expectedResponses = 0;
+
+            data.forEach(reservation => {
+
+                let year = Number(reservation["year"]);
+                let month = Number(monthNumber[reservation["month"]]) - 1;
+                let day = Number(reservation["day"]);
+                
+                let date = new Date(year, month, day);
+                let currentDate = new Date();
+                console.log(date);
+
+                if (date < currentDate){
+                    expectedResponses++;
+                    fetch('http://localhost:3000/busy-time-intervals/' + reservation["id"], {
+                        method: 'delete',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        }
+                    }).then(function(response){
+                        window.location.reload();
+                    })
+                }
+            });
+
+        })
+    
+    })
+
+}
